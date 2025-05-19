bdd1ad1ea38fc4b251739a2f203b812ccada55c7
shubham-ft <139536877+shubham-fantv@users.noreply.github.com>
2025-05-20 02:16:06 +0530

    build fix

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,9 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
-import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
-
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
+      <Html lang="en">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
